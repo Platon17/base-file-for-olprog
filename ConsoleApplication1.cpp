ee7c9c80d5e05553850d479e15051cb32700c798
Platon17 <67402321+Platon17@users.noreply.github.com>
2025-04-04 18:58:56 +1000

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,1 +1,30 @@
+//Platon17 template codeforces
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i,a,n)     for(int i=a;i<n;i++)
+#define pb             push_back
+#define all(v)         v.begin(),v.end()
+#define endl           "\n"
+#define x              first
+#define y              second
+#define sz(a)          (int)a.size()
+#define pii            pair<int,int>
 
+
+
+void solve() {
+
+}
+
+
+
+signed main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int t = 1;
+	//cin >> t;
+	while (t--)
+		solve();
+}
